c444e6500511b55804946b4154f47190186a58ec
Michal Dzurík <misko7104@gmail.com>
2025-03-16 11:09:08 +0100

    About page update

--- src/routes/About.tsx
+++ src/routes/About.tsx
@@ -2,7 +2,23 @@ export default function About() {
     return (
         <>
             <h1 className='heading'>About Project</h1>
-            <p>Example text</p>
+            <p className='mb-4'>
+                Have you ever wondered how can you be more of this guy?
+            </p>
+            <img src="/actually.png" className='h-40 mx-auto mb-4' alt="actually"/>
+            <h2 className='heading mb-2'>Yes?</h2>
+            <p>
+                Well, you've come to the right place. <br/><br/>
+
+                P.S. Be a dear and shower more than twice a year...
+            </p>
+            <br/>
+
+            <h2 className='heading mb-2'>No?</h2>
+            <p>
+                Well, nobody cares.
+            </p>
+            <br/>
         </>
     );
 }
--- src/routes/Calculator.tsx
+++ src/routes/Calculator.tsx
@@ -2,7 +2,7 @@ export default function Calculator() {
     return (
         <>
             <h1 className='heading'>Calculator</h1>
-            <p>Implement calc</p>
+
         </>
     );
 }
